f81749d8941a552f98142031c847514819cc3ae9
Vidit <viditmodi2001@gmail.com>
2023-11-23 16:05:29 +0530

    fixed the family info error

--- classes/applicant.class.js
+++ classes/applicant.class.js
@@ -90,60 +90,72 @@ class Applicant {
 
       case "family":
         const { father, mother, guardian } = data;
+        console.log(data);
+
+        let family_info = {
+          father: {},
+          mother: {},
+          guardian: {},
+        };
 
         if (father) {
+          // console.log("1");
+          // console.log(father);
           if (father.name) {
+            // console.log("2");
             const nameObject = formatName(father.name);
-            this.family_info.father.first_name = nameObject.first_name;
-            this.family_info.father.last_name = nameObject.last_name;
+            // console.log(nameObject);
+
+            family_info.father.first_name = nameObject.first_name;
+            family_info.father.last_name = nameObject.last_name;
 
             if (nameObject.middle_name) {
-              this.family_info.father.middle_name = nameObject.middle_name;
+              family_info.father.middle_name = nameObject.middle_name;
             }
           }
 
           if (father.email) {
-            this.family_info.father.email = father.email;
+            family_info.father.email = father.email;
           }
 
           if (father.contact) {
-            this.family_info.father.contact = father.contact;
+            family_info.father.contact = father.contact;
           }
         }
 
         if (mother) {
           if (mother.name) {
             const nameObject = formatName(mother.name);
-            this.family_info.mother.first_name = nameObject.first_name;
-            this.family_info.mother.last_name = nameObject.last_name;
+            family_info.mother.first_name = nameObject.first_name;
+            family_info.mother.last_name = nameObject.last_name;
 
             if (nameObject.middle_name) {
-              this.family_info.mother.middle_name = nameObject.middle_name;
+              family_info.mother.middle_name = nameObject.middle_name;
             }
           }
 
           if (mother.email) {
-            this.family_info.mother.email = mother.email;
+            family_info.mother.email = mother.email;
           }
 
           if (mother.contact) {
-            this.family_info.mother.contact = mother.contact;
+            family_info.mother.contact = mother.contact;
           }
         }
 
         if (guardian) {
           if (guardian.name) {
             const nameObject = formatName(guardian.name);
-            this.family_info.guardian.first_name = nameObject.first_name;
-            this.family_info.guardian.last_name = nameObject.last_name;
+            family_info.guardian.first_name = nameObject.first_name;
+            family_info.guardian.last_name = nameObject.last_name;
 
             if (nameObject.middle_name) {
-              this.family_info.guardian.middle_name = nameObject.middle_name;
+              family_info.guardian.middle_name = nameObject.middle_name;
             }
           }
 
           if (guardian.relation) {
-            this.family_info.guardian.relation = guardian.relation;
+            family_info.guardian.relation = guardian.relation;
           }
 
           if (guardian.office_address) {
@@ -154,133 +166,140 @@ class Applicant {
               guardian.office_address.district &&
               guardian.office_address.state
             ) {
-              this.family_info.guardian.office_address =
-                guardian.office_address;
+              family_info.guardian.office_address = guardian.office_address;
             }
           }
 
           if (guardian.occupation) {
-            this.family_info.guardian.occupation = guardian.occupation;
+            family_info.guardian.occupation = guardian.occupation;
           }
 
           if (guardian.designation) {
-            this.family_info.guardian.designation = guardian.designation;
+            family_info.guardian.designation = guardian.designation;
           }
 
           if (guardian.office_contact) {
-            this.family_info.guardian.office_contact = guardian.office_contact;
+            family_info.guardian.office_contact = guardian.office_contact;
           }
 
           if (guardian.contact) {
-            this.family_info.guardian.contact = guardian.contact;
+            family_info.guardian.contact = guardian.contact;
           }
           if (guardian.income) {
-            this.family_info.guardian.income = guardian.income;
+            family_info.guardian.income = guardian.income;
           }
           if (guardian.email) {
-            this.family_info.guardian.email = guardian.email;
+            family_info.guardian.email = guardian.email;
           }
           if (guardian.aadhar_number) {
-            this.family_info.guardian.aadhar_number = guardian.aadhar_number;
+            family_info.guardian.aadhar_number = guardian.aadhar_number;
           }
           if (guardian.pan_number) {
-            this.family_info.guardian.pan_number = guardian.pan_number;
+            family_info.guardian.pan_number = guardian.pan_number;
           }
         }
+
+        this.family_info = family_info;
         break;
 
       case "academic":
-        const {admission, secondary, higher_secondary} = data
+        const { admission, secondary, higher_secondary } = data;
 
-        if(admission){
-            if(admission.exam_name){
-                this.academic_info.admission.exam_name = admission.exam_name
-            }
-            if(admission.year_of_exam){
-                this.academic_info.admission.year_of_exam = admission.year_of_exam
-            }
-            if(admission.roll_number){
-                this.academic_info.admission.roll_number = admission.roll_number
-            }
-            if(admission.rank){
-                this.academic_info.admission.rank = admission.rank
-            }
+        if (admission) {
+          if (admission.exam_name) {
+            this.academic_info.admission.exam_name = admission.exam_name;
+          }
+          if (admission.year_of_exam) {
+            this.academic_info.admission.year_of_exam = admission.year_of_exam;
+          }
+          if (admission.roll_number) {
+            this.academic_info.admission.roll_number = admission.roll_number;
+          }
+          if (admission.rank) {
+            this.academic_info.admission.rank = admission.rank;
+          }
         }
 
-        if(secondary){
-            if(secondary.exam_name){
-                this.academic_info.secondary.exam_name = secondary.exam_name
-            }
-            if(secondary.year_of_exam){
-                this.academic_info.secondary.year_of_exam = secondary.year_of_exam
-            }
-            if(secondary.board){
-                this.academic_info.secondary.board = secondary.board
-            }
-            if(secondary.aggregate){
-                this.academic_info.secondary.aggregate = secondary.aggregate
-            }
-            if(secondary.school_name){
-                this.academic_info.secondary.school_name = secondary.school_name
-            }
-            if(secondary.subjects){
-                let subjectsArray = subjects.split(",")
-                let subjects="", marks={}
-                subjectsArray.forEach(subject => {
-                    subject = subject.split("-")
-                    subjects = subjects + subject[0];
-                    marks[subject[0]] = subject[1]
-                });
+        if (secondary) {
+          if (secondary.exam_name) {
+            this.academic_info.secondary.exam_name = secondary.exam_name;
+          }
+          if (secondary.year_of_exam) {
+            this.academic_info.secondary.year_of_exam = secondary.year_of_exam;
+          }
+          if (secondary.board) {
+            this.academic_info.secondary.board = secondary.board;
+          }
+          if (secondary.aggregate) {
+            this.academic_info.secondary.aggregate = secondary.aggregate;
+          }
+          if (secondary.school_name) {
+            this.academic_info.secondary.school_name = secondary.school_name;
+          }
+          if (secondary.subjects) {
+            let subjectsArray = subjects.split(",");
+            let subjects = "",
+              marks = {};
+            subjectsArray.forEach((subject) => {
+              subject = subject.split("-");
+              subjects = subjects + subject[0];
+              marks[subject[0]] = subject[1];
+            });
 
-                this.academic_info.secondary.subjects = subjects;
-                this.academic_info.secondary.marks = marks
-            }
+            this.academic_info.secondary.subjects = subjects;
+            this.academic_info.secondary.marks = marks;
+          }
         }
 
-        if(higher_secondary){
-            if(higher_secondary.exam_name){
-                this.academic_info.higher_secondary.exam_name = higher_secondary.exam_name
-            }
-            if(higher_secondary.year_of_exam){
-                this.academic_info.higher_secondary.year_of_exam = higher_secondary.year_of_exam
-            }
-            if(higher_secondary.board){
-                this.academic_info.higher_secondary.board = higher_secondary.board
-            }
-            if(higher_secondary.aggregate){
-                this.academic_info.higher_secondary.aggregate = higher_secondary.aggregate
-            }
-            if(higher_secondary.school_name){
-                this.academic_info.higher_secondary.school_name = higher_secondary.school_name
-            }
-            if(higher_secondary.subjects){
-                let subjectsArray = subjects.split(",")
-                let subjects="", marks={}
-                subjectsArray.forEach(subject => {
-                    subject = subject.split("-")
-                    subjects = subjects + subject[0];
-                    marks[subject[0]] = subject[1]
-                });
+        if (higher_secondary) {
+          if (higher_secondary.exam_name) {
+            this.academic_info.higher_secondary.exam_name =
+              higher_secondary.exam_name;
+          }
+          if (higher_secondary.year_of_exam) {
+            this.academic_info.higher_secondary.year_of_exam =
+              higher_secondary.year_of_exam;
+          }
+          if (higher_secondary.board) {
+            this.academic_info.higher_secondary.board = higher_secondary.board;
+          }
+          if (higher_secondary.aggregate) {
+            this.academic_info.higher_secondary.aggregate =
+              higher_secondary.aggregate;
+          }
+          if (higher_secondary.school_name) {
+            this.academic_info.higher_secondary.school_name =
+              higher_secondary.school_name;
+          }
+          if (higher_secondary.subjects) {
+            let subjectsArray = subjects.split(",");
+            let subjects = "",
+              marks = {};
+            subjectsArray.forEach((subject) => {
+              subject = subject.split("-");
+              subjects = subjects + subject[0];
+              marks[subject[0]] = subject[1];
+            });
 
-                this.academic_info.higher_secondary.subjects = subjects;
-                this.academic_info.higher_secondary.marks = marks
-            }
+            this.academic_info.higher_secondary.subjects = subjects;
+            this.academic_info.higher_secondary.marks = marks;
+          }
         }
         break;
 
       case "course":
-        const {course_name, duration, stream, admission_year} = data;
-        if(course_name){
-            this.course_info.course_name = course_name
+        const { course_name, duration, stream, admission_year } = data;
+        if (course_name) {
+          this.course_info.course_name = course_name;
         }
-        if(duration){
-            this.course_info.duration = duration
+        if (duration) {
+          this.course_info.duration = duration;
         }
-        if(stream){
-            this.course_info.stream = stream
+        if (stream) {
+          this.course_info.stream = stream;
         }
-        if(admission_year){
-            this.course_info.admission_year = admission_year
+        if (admission_year) {
+          this.course_info.admission_year = admission_year;
         }
         break;
 
